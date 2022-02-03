547182b5f476984c1bfb172b27c5ba9d362802ec
IvashchenkoIvanIgorevich <ivachko2009@gmail.com>
2022-02-03 18:33:01 +0200

    Add project files.

---
+++ VideoSearchService.UI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VideoSearchService.UI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
